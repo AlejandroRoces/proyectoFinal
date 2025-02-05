799d47aba7e51b1c8f8de07b8dda3f63b5a21d41
alesroces03 <aleroces@gmail.com>
2025-02-05 23:52:01 +0100

    dash con graficos funcionando

--- assets/css/globales.php
+++ assets/css/globales.php
@@ -4,14 +4,14 @@ header("Content-Type: text/css"); // Indica que este archivo devuelve CSS
 require_once "connection.php"; // Asegura que se conecte a la base de datos
 
 $conexion = conectarDB();
-$query = "SELECT * FROM tu_tabla LIMIT 1"; // Reemplaza 'tu_tabla' con el nombre real de la tabla
+$query = "SELECT * FROM configuracion_camptrack LIMIT 1"; 
 $stmt = $conexion->query($query);
 $config = $stmt->fetch(PDO::FETCH_ASSOC);
 
 ?>
 
 :root {
-    --color-header: <?php echo $config['color_header']; ?>;
+    --color-header:<?php echo $config['color_header']; ?>;
     --color-nav: <?php echo $config['color_nav']; ?>;
     --color-fondo: <?php echo $config['color_fondo']; ?>;
     --color-botones: <?php echo $config['color_botones']; ?>;
